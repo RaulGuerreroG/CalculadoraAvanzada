856e562b68b21450b8cd5a5d32ada98d8f408998
RaulGuerreroG <solucionesicg@gmail.com>
2023-09-09 12:10:16 -0400

    Inicio Programacion

--- ProgramaPractica/Program.cs
+++ ProgramaPractica/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+   Hacer un programa que le pida al usuario su nombre
+    y su apellido, el nombre y el apellido, guardarlos
+    en variables separadas.
+
+    Luego, preguntarle al usuario por su sexo: Femenino (F) o 
+    Masculino (M).
+
+    Luego, preguntarle la edad al usuario. Si es menor de 18, decirle que
+    no puede usar el sistema. Si tiene 18 años en adelante, darle la 
+    darle la bienvenida al usuario. Considera su sexo para decirle:
+    Bienvenido o Bienvenida.
+
+    Luego, pidele al usuario llamandolo por su nombre completo, que ingrese
+    dos numeros enteros.
+
+    Luego, pidele que te indique que operacion matematica quiere hacer: 
+    Presentale las diferentes opciones que son:
+    Suma (s) - Resta (r) - Multiplicacion (M) - Division (d)
+    Exponente (e)
+
+    Por ultimo: Mostrar en pantalla el resultado final, diciendole:
+    Usuario: El resultado de la operacion X es Y
+ 
+ 
+ 
+ 
+ */
+
+Console.WriteLine("Digita tu nombre");
+string name = string.Empty;
